5953fd0fa91108f0cbf590d2cce5ace208d7abe8
SumSumCorn <a01035587180@gmail.com>
2022-04-06 07:56:33 +0900

    clap 적용

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,66 @@
+use clap::{Arg, Command};
 use std::env;
-use std::io::{self, Read, Write};
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
-    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+fn main() -> Result<()> {
+    let matches = Command::new("pipeviewer")
+        .arg(Arg::new("infile").help("Read from a file instead of stdin"))
+        .arg(
+            Arg::new("outfile")
+                .short('o')
+                .long("outfile")
+                .takes_value(true)
+                .help("Write output to a file instead of stdout"),
+        )
+        .arg(Arg::new("silent").short('s').long("silent"))
+        .get_matches();
+    let infile = matches.value_of("infile").unwrap_or_default();
+    let outfile = matches.value_of("outfile").unwrap_or_default();
+    let silent = if matches.is_present("silent") {
+        true
+    } else {
+        !env::var("PV_SILENT").unwrap_or_default().is_empty()
+    };
+
+    let mut reader: Box<dyn Read> = if !infile.is_empty() {
+        Box::new(BufReader::new(File::open(infile)?))
+    } else {
+        Box::new(BufReader::new(io::stdin()))
+    };
+
+    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
+        Box::new(BufWriter::new(File::create(outfile)?))
+    } else {
+        Box::new(BufWriter::new(io::stdout()))
+    };
 
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
-        let num_read = match io::stdin().read(&mut buffer) {
+        let num_read = match reader.read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
         };
         total_bytes += num_read;
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
         // eprintln!("num_read: {}", num_read);
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if let Err(e) = writer.write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+            // eprintln!("oh no an error! {}", e.to_string());
+            // std::process::exit(1);
+        }
     }
     if !silent {
-        eprintln!("total_bytes: {}", total_bytes);
+        eprintln!("\r{}", total_bytes);
     }
+    Ok(())
 }
